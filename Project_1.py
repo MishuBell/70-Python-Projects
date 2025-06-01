ef3b183a8e1730fb400d6136fa99d533dc9b86e2
Mishu Bellringer <128057567+MishuBell@users.noreply.github.com>
2025-06-01 13:20:59 +0200

    Update Project_1.py

--- Project_1.py
+++ Project_1.py
@@ -4,18 +4,6 @@ import tkinter
 # https://pythongeeks.org/python-mad-libs-generator-project/
 
 
-# "Mad Libs is a word game created by Leonard Stern and Roger Price. It consists of one player prompting
-# others for a list of words to substitute for blanks in a story before reading aloud."
-# https://en.wikipedia.org/wiki/Mad_Libs
-
-# "Frankenstein; Or, The Modern Prometheus" by Mary Wollstonecraft Shelley is a novel
-# written in the early 19th century.
-# https://www.gutenberg.org/ebooks/84
- 
-# Needs:
-# Adjective, Adverb, Noun, Verb, Tense, Place,
-# Exclamation, Silly Word, Number, Color, Animal, Part of the Body, Plurals
-
 Screen = tkinter.Tk()
 Screen.title("PythonGeeks Mad Libs Generator")
 Screen.geometry('400x400')
